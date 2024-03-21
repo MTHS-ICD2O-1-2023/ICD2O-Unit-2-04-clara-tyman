9b2b01c31f641ca99e4deb873f13a7cd4a4dce73
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-21 17:48:45 +0000

    done maybe?

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara Tyman All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculateAreaOfRectangle () {
+  // input
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+  const height = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const area = base * height / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
